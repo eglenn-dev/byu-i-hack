ebf2af83afbdde05f8122205eaa4f0fffa9ab4ba
Ethan Glenn <ethan+git@eglenn.dev>
2024-10-18 21:59:04 -0600

    Updated to preven prefetching of the auth pageS

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-import Link from "next/link";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+
+  const session = await getSession();
+  if (session) redirect('/dashboard');
 
   return (
     <div>
       <h1>Home</h1>
       <p>Home page content</p>
-      <Link href={'/login'}>Login</Link>
-      <Link href={'/signup'}>Signup</Link>
+      <a href={'/login'}>Login</a>
+      <a href={'/signup'}>Signup</a>
     </div>
   );
 }
