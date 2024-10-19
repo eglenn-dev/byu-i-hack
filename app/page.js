d0f4a39c77f96199858504d112de0ffdc5441082
Ethan Glenn <ethan+git@eglenn.dev>
2024-10-19 07:37:40 -0600

    Updated home page styleS

--- app/page.js
+++ app/page.js
@@ -7,11 +7,11 @@ export default async function Home() {
   if (session) redirect('/dashboard');
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Home page content</p>
-      <a href={'/login'}>Login</a>
-      <a href={'/signup'}>Signup</a>
+    <div className="flex flex-col gap-6 w-full h-screen items-center justify-center p-4">
+      <h1 className="text-5xl text-center">Welcome to NeighborView</h1>
+      <p className="text-3xl text-center">Keep our neighborhoods safe together</p>
+      <a className="pt-2 pb-2 pl-4 pr-4 bg-green-700 rounded-2xl" href={'/login'}>Login</a>
+      <a href={'/signup'}>Create an Account</a>
     </div>
   );
 }
