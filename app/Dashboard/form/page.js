0a7f540a2ef38011afcbea1bbbfed644992c0c77
Ethan Glenn <ethan+git@eglenn.dev>
2024-10-19 04:29:55 -0600

    Updated form styles and global fonts

--- app/Dashboard/form/page.js
+++ app/Dashboard/form/page.js
@@ -7,8 +7,9 @@ export default async function FormPage() {
     if (!session) return redirect("/login");
 
     return (
-        <div>
-            <h1>Completion Form</h1>
+        <div className="flex flex-col items-center w-full">
+            <h1 className="text-5xl m-10 font-bold">Report Form</h1>
+            <div className="text-center mb-6">Please be aware that these form submissions are not public, and all data used is slightly modified to hide identity.</div>
             <SubmissionForm />
         </div>
     );
--- app/ui/submissionForm.js
+++ app/ui/submissionForm.js
@@ -11,75 +11,78 @@ export default function SubmissionForm() {
     };
 
     return (
-        <div>
-            <form className="flex flex-col gap-4" onSubmit={(e) => {
-                e.preventDefault();
-                setLoading(true);
-                createPost(new FormData(e.target));
-            }}>
-                <label>
-                    <span>Location</span>
-                    <input required type="text" id="location" name="location" placeholder="123 Street, ST 12345" />
+        <form className="flex flex-col gap-4 w-72" onSubmit={(e) => {
+            e.preventDefault();
+            setLoading(true);
+            createPost(new FormData(e.target));
+        }}>
+            <label className="flex flex-col">
+                <span>Location</span>
+                <input className="h-11 p-2 rounded-full" required type="text" id="location" name="location" placeholder="123 Street, ST 12345" />
+            </label>
+            <div className="flex flex-col gap-4">
+                <label className="mr-auto ml-auto">
+                    <input
+                        required
+                        type="radio"
+                        id="vehicle"
+                        name="type"
+                        value="vehicle"
+                        checked={type === "vehicle"}
+                        onChange={handleTypeChange}
+                    />
+                    Vehicle
                 </label>
-                <div className="flex flex-col gap-4">
-                    <label>
-                        <input
-                            required
-                            type="radio"
-                            id="vehicle"
-                            name="type"
-                            value="vehicle"
-                            checked={type === "vehicle"}
-                            onChange={handleTypeChange}
-                        />
-                        Vehicle
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            id="neighborhood"
-                            name="type"
-                            value="neighborhood"
-                            checked={type === "neighborhood"}
-                            onChange={handleTypeChange}
-                        />
-                        Neighborhood
-                    </label>
-                    <label>
-                        Description
-                        <textarea required style={{ resize: "none" }} id="description" name="description" placeholder="Tell us about what happened..." />
-                    </label>
-                    <label>
-                        Date of Incident
-                        <input required type="date" id="date" name="date" />
+                <label className="mr-auto ml-auto">
+                    <input
+                        type="radio"
+                        id="neighborhood"
+                        name="type"
+                        value="neighborhood"
+                        checked={type === "neighborhood"}
+                        onChange={handleTypeChange}
+                    />
+                    Neighborhood
+                </label>
+                <label className="flex flex-col">
+                    Description
+                    <textarea className="h-48 p-3 w-82 rounded-3xl" required style={{ resize: "none" }} id="description" name="description" placeholder="Tell us about what happened..." />
+                </label>
+                <label className="flex flex-col">
+                    Date of Incident
+                    <input required type="date" id="date" name="date" />
+                </label>
+            </div>
+            {type === "vehicle" ? (
+                <div>
+                    <label className="flex flex-col">
+                        License Plate
+                        <input required={type === "vehicle"} type="text" id="license" name="license" />
                     </label>
                 </div>
-                {type === "vehicle" ? (
-                    <div>
-                        <label>
-                            License Plate
-                            <input required={type === "vehicle"} type="text" id="license" name="license" />
-                        </label>
-                    </div>
-                ) : (type === "neighborhood" ? (
-                    <div>
-                        <label>
-                            Category
-                            <select required={type === "neighborhood"} id="category" name="category">
-                                <option value="">Select one</option>
-                                <option value="domestic-violence">Domestic Violence</option>
-                                <option value="vandalism">Vandalism</option>
-                                <option value="theft">Theft</option>
-                                <option value="burglary">Burglary</option>
-                                <option value="noise">Noise Complaint</option>
-                                <option value="animal">Animal Control</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </label>
-                    </div>
-                ) : "")}
-                <button disabled={loading} type="submit">{loading ? "Loading..." : "Submit Form"}</button>
-            </form>
-        </div>
+            ) : (type === "neighborhood" ? (
+                <div>
+                    <label className="flex flex-col">
+                        Category
+                        <select required={type === "neighborhood"} id="category" name="category">
+                            <option value="">Select one</option>
+                            <option value="domestic-violence">Domestic Violence</option>
+                            <option value="vandalism">Vandalism</option>
+                            <option value="theft">Theft</option>
+                            <option value="burglary">Burglary</option>
+                            <option value="noise">Noise Complaint</option>
+                            <option value="animal">Animal Control</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </label>
+                </div>
+            ) : "")}
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                disabled={loading}
+                type="submit">
+                {loading ? "Loading..." : "Submit Form"}
+            </button>
+        </form>
     );
 }
