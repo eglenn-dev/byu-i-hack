2ee5ecdde83dc6cf4d3a8d0def43f1397234ec7f
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-10-19 02:11:47 -0600

    no more dynamic

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,16 +1,12 @@
 // pages/map.js
-import dynamic from "next/dynamic";
-import Map from "./Map";
-
-// Dynamically import the MapComponent with SSR disabled
-const MapComponent = dynamic(() => Map, { ssr: false });
+import MapComponent from "./Map";
 
 const MapPage = () => {
   const API_KEY = process.env.GOOGLE_MAPS_API_KEY; // Accessing the API key from environment variables
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <MapComponent apiKey={API_KEY} />
+      <MapComponent apiKey={API_KEY} /> {/* Render the MapComponent directly */}
     </div>
   );
 };
